cfb089643374d0cab65f11d739413deb465b250f
godstealingleader <107918254+godstealingleader@users.noreply.github.com>
2023-05-03 19:25:25 +0800

    Add files via upload

---
+++ 2023416.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//#include <string>
+//
+//int main()
+//{
+//	using namespace std;
+//	//const int name_lenth = 20;
+//	string First_name;
+//	string Last_name;
+//	cout << "please enter your frist name:"; getline(cin,First_name);
+//	cout << "please enter your last name:"; getline(cin,Last_name);
+//	cout << "hello! " << Last_name << ", " << First_name;
+//	return 0;
+//}
